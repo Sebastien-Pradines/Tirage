081084607918226f6a87214901237412ea8cab0a
Florian <florian.lprt@gmail.com>
2020-06-10 09:36:54 +0200

    update test

--- test/suites/ExamplePlayer.test.js
+++ test/suites/ExamplePlayer.test.js
@@ -7,6 +7,8 @@ describe('ExamplePlayer test suite', () => {
   test('Player should grow 5px by 5px', () => {
     const player = new ExamplePlayer();
     expect(player.r).toBe(5);
+    player.grow();
+    expect(player.r).toBe(10);
   });
 
 });
